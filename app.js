f725c4a7b16a35f591cb76e0e759c8313a0e3131
Hamza Ilyas <hamza24061@gmail.com>
2021-10-08 20:32:51 +0500

    firstcommit

---
+++ app.js
@@ -0,0 +1,8 @@
+function doSum() {
+    var num1 =  document.getElementById("firstnumber").value;
+    var num2 = document.getElementById("secondnumber").value;
+    var result = document.getElementById("result");
+
+    var sum = Number(num1) + Number(num2);
+
+}
