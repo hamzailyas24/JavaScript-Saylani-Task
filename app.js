8a3c753cfea34059742f1ed19a4587319e6e642d
Hamza Ilyas <hamza24061@gmail.com>
2021-10-08 21:19:25 +0500

    task completed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function doSum() {
     var num1 =  document.getElementById("firstnumber").value;
     var num2 = document.getElementById("secondnumber").value;
-    var result = document.getElementById("result");
-
+    
     var sum = Number(num1) + Number(num2);
-
+    
+    var result = document.getElementById("result").innerText = sum;
 }
