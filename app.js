f914816f5f0372a53eec7f1c4455a75ed1099d6d
Muhammad zubait <zubairsaleem1210@gmail.com>
2021-10-11 21:57:00 +0500

    Final

--- app.js
+++ app.js
@@ -7,9 +7,6 @@
 //     var result = document.getElementById("result").innerText = sum;
 // }
 
-
-
-
 function convertusd() {
     var pkr = document.getElementById("pkr").value;
     
@@ -25,5 +22,63 @@ function convertpkr() {
     let result = Number(usd) / 170;
 
     document.getElementById("pkrpara").innerHTML = result;
+
+}
+
+function jacket() {
+    let weather = document.getElementById("weather").value;
+
+    
+    if(weather >= 25 ){
+
+        let weatherpara = document.getElementById("weatherpara").innerText = "Dont carry jacket"
+    
+    }
+
+    else if(weather <= 15){
+
+        let weatherpara = document.getElementById("weatherpara").innerText = "carry coat"
+    
+    }
+
+    else if(weather <= 0){
+
+        let weatherpara = document.getElementById("weatherpara").innerText = "Dont Go Out"
+    
+    }
+
+    else if(weather < 25){
+
+        let weatherpara = document.getElementById("weatherpara").innerText = "carry jacket"
+    
+    }
+
+}
+
+function checkgrade() {
+
+let per = document.getElementById("gradeinput").value;
+
+if (per >= 80 && per <= 100) {
+    let gradepara = document.getElementById("gradepara").innerText = "A-One Grade"; 
+}
+else if (per >= 70 && per < 80) {
+    let gradepara = document.getElementById("gradepara").innerText = "A Grade";
+}
+else if (per >= 60 && per < 70) {
+    let gradepara = document.getElementById("gradepara").innerText = "B Grade";
+}
+else if (per <= 60 && per>=50) {
+    let gradepara = document.getElementById("gradepara").innerText = "C Grade";
+}
+else if (per < 50 && per >= 40) {
+    let gradepara = document.getElementById("gradepara").innerText = "D Grade";
+}
+else if (per < 40) {
+    let gradepara = document.getElementById("gradepara").innerText = "You're Fail Sorry";
+}
+else {
+    alert ("Invalid number");
+}
 
 }
