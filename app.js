ff1e1cfd463c5e7c7476cfc54535c1c5d0180185
Muhammad zubait <zubairsaleem1210@gmail.com>
2021-10-11 21:22:03 +0500

    Currency Convert

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-function doSum() {
-    var num1 =  document.getElementById("firstnumber").value;
-    var num2 = document.getElementById("secondnumber").value;
+// function doSum() {
+//     var num1 =  document.getElementById("firstnumber").value;
+//     var num2 = document.getElementById("secondnumber").value;
     
-    var sum = Number(num1) + Number(num2);
+//     var sum = Number(num1) + Number(num2);
     
-    var result = document.getElementById("result").innerText = sum;
+//     var result = document.getElementById("result").innerText = sum;
+// }
+
+
+
+
+function convertusd() {
+    var pkr = document.getElementById("pkr").value;
+    
+    let result = Number(pkr) * 170;
+
+    document.getElementById("usdpara").innerHTML = result
+
+}
+
+function convertpkr() {
+    var usd = document.getElementById("usd").value;
+
+    let result = Number(usd) / 170;
+
+    document.getElementById("pkrpara").innerHTML = result;
+
 }
