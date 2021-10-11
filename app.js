6c4f534e4483fe4030ce0f853661891cc52a8f4e
Muhammad zubait <zubairsaleem1210@gmail.com>
2021-10-11 22:46:37 +0500

    Task Completed

--- app.js
+++ app.js
@@ -7,78 +7,85 @@
 //     var result = document.getElementById("result").innerText = sum;
 // }
 
-function convertusd() {
-    var pkr = document.getElementById("pkr").value;
+// function convertusd() {
+//     var pkr = document.getElementById("pkr").value;
     
-    let result = Number(pkr) * 170;
+//     let result = Number(pkr) / 170;
 
-    document.getElementById("usdpara").innerHTML = result
+//     document.getElementById("usdpara").innerHTML = result
 
-}
+// }
 
-function convertpkr() {
-    var usd = document.getElementById("usd").value;
+// function convertpkr() {
+//     var usd = document.getElementById("usd").value;
 
-    let result = Number(usd) / 170;
+//     let result = Number(usd) * 170;
 
-    document.getElementById("pkrpara").innerHTML = result;
+//     document.getElementById("pkrpara").innerHTML = result;
 
-}
+// }
 
-function jacket() {
-    let weather = document.getElementById("weather").value;
+// function jacket() {
+//     let weather = document.getElementById("weather").value;
 
     
-    if(weather >= 25 ){
+//     if(weather >= 25 ){
 
-        let weatherpara = document.getElementById("weatherpara").innerText = "Dont carry jacket"
+//         let weatherpara = document.getElementById("weatherpara").innerText = "Dont carry jacket"
     
-    }
+//     }
 
-    else if(weather <= 15){
+//     else if(weather <= 15){
 
-        let weatherpara = document.getElementById("weatherpara").innerText = "carry coat"
+//         let weatherpara = document.getElementById("weatherpara").innerText = "carry coat"
     
-    }
+//     }
 
-    else if(weather <= 0){
+//     else if(weather <= 0){
 
-        let weatherpara = document.getElementById("weatherpara").innerText = "Dont Go Out"
+//         let weatherpara = document.getElementById("weatherpara").innerText = "Dont Go Out"
     
-    }
+//     }
 
-    else if(weather < 25){
+//     else if(weather < 25){
 
-        let weatherpara = document.getElementById("weatherpara").innerText = "carry jacket"
+//         let weatherpara = document.getElementById("weatherpara").innerText = "carry jacket"
     
-    }
+//     }
 
-}
+// }
 
-function checkgrade() {
+// function checkgrade() {
 
-let per = document.getElementById("gradeinput").value;
+// let per = document.getElementById("gradeinput").value;
 
-if (per >= 80 && per <= 100) {
-    let gradepara = document.getElementById("gradepara").innerText = "A-One Grade"; 
-}
-else if (per >= 70 && per < 80) {
-    let gradepara = document.getElementById("gradepara").innerText = "A Grade";
-}
-else if (per >= 60 && per < 70) {
-    let gradepara = document.getElementById("gradepara").innerText = "B Grade";
-}
-else if (per <= 60 && per>=50) {
-    let gradepara = document.getElementById("gradepara").innerText = "C Grade";
-}
-else if (per < 50 && per >= 40) {
-    let gradepara = document.getElementById("gradepara").innerText = "D Grade";
-}
-else if (per < 40) {
-    let gradepara = document.getElementById("gradepara").innerText = "You're Fail Sorry";
-}
-else {
-    alert ("Invalid number");
-}
+// if (per >= 80 && per <= 100) {
+//     let gradepara = document.getElementById("gradepara").innerText = "A-One Grade"; 
+// }
+// else if (per >= 70 && per < 80) {
+//     let gradepara = document.getElementById("gradepara").innerText = "A Grade";
+// }
+// else if (per >= 60 && per < 70) {
+//     let gradepara = document.getElementById("gradepara").innerText = "B Grade";
+// }
+// else if (per <= 60 && per>=50) {
+//     let gradepara = document.getElementById("gradepara").innerText = "C Grade";
+// }
+// else if (per < 50 && per >= 40) {
+//     let gradepara = document.getElementById("gradepara").innerText = "D Grade";
+// }
+// else if (per < 40) {
+//     let gradepara = document.getElementById("gradepara").innerText = "You're Fail Sorry";
+// }
+// else {
+//     alert ("Invalid number");
+// }
 
+// }
+
+for(var i=1; i<=10; i++){
+
+    var res = 2*i;
+
+    console.log(res + "");
 }
